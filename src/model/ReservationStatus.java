1cbc35fdc6152f35e4ba45747dc031161e762374
Giovana Jacome Marchetti <g177700@dac.unicamp.br>
2025-06-29 02:20:05 -0300

    adiciona pagina gerenciar reservas

---
+++ src/model/ReservationStatus.java
@@ -0,0 +1,15 @@
+package model;
+// Exemplo de enumeração 
+public enum ReservationStatus {
+    CONFIRMED("Confirmada"), PENDING("Pendente"), CANCELLED("Cancelada"), REJECTED("Rejeitada");
+
+    private String name;
+
+    ReservationStatus(String name){
+        this.name=name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
